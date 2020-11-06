9ea77a8898ee1dc90f342ab2f9298664e73bf1e6
Gabriel Ruivo <gabriel-ruivo@hotmail.com>
2020-11-06 00:47:06 -0300

    resolution-32

--- 29-javascript-gabriel-ruivo.js
+++ 29-javascript-gabriel-ruivo.js
@@ -31,90 +31,171 @@
     que será nomeado de "app".
     */
 
-  var app =(
-    function () {
-      return {
-        init: function () {
-          this.getCompanyData();
-          this.initEvents();
-        },
-  
-        getCompanyData: function getCompanyData() {
-          var ajax = new XMLHttpRequest();
-  
-          ajax.open("GET", "company.json");
-          ajax.send();
-          ajax.addEventListener("readystatechange", this.setCompanyData, false);
-        },
-  
-        setCompanyData: function setCompanyData() {
-          if (app.isRequestOk.call(this)) {
-            var $companyName = DOM("[data-js=companyName]");
-            var $companyPhone = DOM("[data-js=companyPhone]");
-            var parsedData = JSON.parse(this.responseText);
-            $companyName.get().textContent = parsedData.name;
-            $companyPhone.get().textContent = parsedData.phone;
-          }
-        },
-  
-        isRequestOk: function isRequestOk() {
-          return this.readyState === 4 && this.status === 200;
-        },
-  
-        initEvents: function initEvents () {
-          var $carForm = DOM("[data-js=carForm]");
-          $carForm.on("submit", this.handleSubmit);
-        },
-  
-        handleSubmit: function handleSubmit(event) {
-          event.preventDefault();
-          var $carTable = DOM("[data-js=carTable]");
-          $carTable.get().appendChild(app.newCar());
-        },
+  let app = (function () {
+    return {
 
-        handleDelete: function handleDelete(event){
-          event.preventDefault()
-          var $deletedRow = this.parentNode.parentNode
-          var $table = $deletedRow.parentNode
-          $table.removeChild($deletedRow)
-        },
-  
-        newCar: function newCar() {
-          var $fragment = doc.createDocumentFragment();
-          var $newTr = doc.createElement("tr");
-          var $tdImage = doc.createElement("td");
-          var $imagem = doc.createElement("img")
-          var $modelTd = doc.createElement("td");
-          var $yearTd = doc.createElement("td");
-          var $plateTd = doc.createElement("td");
-          var $colorTd = doc.createElement("td");
-          var $tdDelete = doc.createElement("td")
-          var $deleteButton = doc.createElement("button");
+      //inicializa as funções;
+      init: function () {
+        this.getCompanyData();
+        this.initEvents();
+        this.getDataFromServer();
+      },
 
-          $deleteButton.innerHTML = "Deletar"
-          $deleteButton.addEventListener('click',this.handleDelete, false);
-          $tdDelete.appendChild($deleteButton);
+      //busca da company.json nome da empresa e telefone
+      getCompanyData: function getCompanyData() {
+        let ajax = new XMLHttpRequest();
+        ajax.open("GET", "company.json");
+        ajax.send();
+        ajax.addEventListener("readystatechange", this.setCompanyData, false);
+      },
 
-          $imagem.src = DOM("[data-js=imagem]").get().value;
-          $tdImage.appendChild($imagem)
-          $modelTd.textContent = DOM("[data-js=modelo]").get().value;
-          $yearTd.textContent = DOM("[data-js=ano]").get().value;
-          $plateTd.textContent = DOM("[data-js=placa]").get().value;
-          $colorTd.textContent = DOM("[data-js=cor]").get().value;
-  
-          $newTr.appendChild($tdImage);
-          $newTr.appendChild($modelTd);
-          $newTr.appendChild($yearTd);
-          $newTr.appendChild($plateTd);
-          $newTr.appendChild($colorTd);
-          $newTr.appendChild($tdDelete);
-  
-          return $fragment.appendChild($newTr);
-        },
-      };
-    }
-  )()
-  
+      //setta os respectivos nomes na tela.
+      setCompanyData: function setCompanyData() {
+        if (app.isRequestOk.call(this)) {
+          let $companyName = DOM("[data-js=companyName]");
+          let $companyPhone = DOM("[data-js=companyPhone]");
+          let parsedData = JSON.parse(this.responseText);
+          $companyName.get().textContent = parsedData.name;
+          $companyPhone.get().textContent = parsedData.phone;
+        }
+      },
+
+      //verifica se a requisição deu tudo certo.(true or false)
+      isRequestOk: function isRequestOk() {
+        return this.readyState === 4 && this.status === 200;
+      },
+
+      //pega dados do carro do servidor;
+      getDataFromServer: function getDataFromServer() {
+        let ajaxGet = new XMLHttpRequest();
+        ajaxGet.open("GET", "http://localhost:3000/car");
+        ajaxGet.send();
+        ajaxGet.addEventListener("readystatechange", this.carData, false);
+      },
+
+      //pega os dados entrados pelo usuario e cria uma nova tabela;
+      carData: function carData() {
+        if (app.isRequestOk.call(this)) {
+          let allItemCars = JSON.parse(this.response);
+          allItemCars.forEach(item => {
+            app.addCarToTable(
+              item.image,
+              item.brandModel,
+              item.year,
+              item.plate,
+              item.color
+            );
+          });
+        }
+      },
+
+      //salva os dados digitados enviando um post para o servidor;
+      saveCarData: function saveCarData() {
+        let $inputValues = this.getInputValues();
+        let ajaxPostCar = new XMLHttpRequest();
+        ajaxPostCar.open("POST", "http://localhost:3000/car");
+        
+        let image = $inputValues.imagem;
+        let brandModel = $inputValues.modelo;        
+        let year = $inputValues.ano;
+        let plate = $inputValues.placa;
+        let color = $inputValues.cor;
+        
+        ajaxPostCar.send(`
+          image=${image}
+          &brandModel=${brandModel}
+          &year=${year}
+          &plate=${plate}
+          &color=${color}`
+        );
+      },
+
+      initEvents: function initEvents() {
+        let $carForm = DOM("[data-js=carForm]");
+        $carForm.on("submit", this.handleSubmit);
+      },
+
+      handleSubmit: function handleSubmit(event) {
+        event.preventDefault();
+        app.saveCarData();
+        app.newCar();
+      },
+
+      handleDelete: function handleDelete(event) {
+        event.preventDefault();
+        console.log('this:', this) // <button>Deletar</button>
+        let $deletedRow = this.parentNode.parentNode; //td --> tr
+        console.log($deletedRow)
+        let $table = $deletedRow.parentNode; // toda table
+        console.log($table);
+        $table.removeChild($deletedRow); // remove apenas filho de table <tr>
+      },
+
+      newCar: function newCar() {
+        let $inputValues = this.getInputValues();
+        this.addCarToTable(
+          $inputValues.imagem,
+          $inputValues.modelo,
+          $inputValues.ano,
+          $inputValues.placa,
+          $inputValues.cor
+        );
+      },
+
+      addCarToTable: function addCarToTable(imagem, modelo, ano, placa, cor) {
+        let $carTable = DOM("[data-js=carTable]");
+
+        let $fragment = doc.createDocumentFragment();
+        let $newTr = doc.createElement("tr");
+        let $imageTd = doc.createElement("td");
+        let $imagem = doc.createElement("img");
+        let $modelTd = doc.createElement("td");
+        let $yearTd = doc.createElement("td");
+        let $plateTd = doc.createElement("td");
+        let $colorTd = doc.createElement("td");
+        let $deleteCell = doc.createElement("td");
+
+        let $deleteButton = doc.createElement("button");
+        $deleteButton.innerHTML = "Deletar";
+        $deleteButton.addEventListener("click", this.handleDelete, false);
+        $deleteCell.appendChild($deleteButton);
+
+        $imagem.src = imagem;
+        $imageTd.appendChild($imagem);
+        $modelTd.textContent = modelo;
+        $yearTd.textContent = ano;
+        $plateTd.textContent = placa;
+        $colorTd.textContent = cor;
+
+        $newTr.appendChild($imageTd);
+        $newTr.appendChild($modelTd);
+        $newTr.appendChild($yearTd);
+        $newTr.appendChild($plateTd);
+        $newTr.appendChild($colorTd);
+        $newTr.appendChild($deleteCell);
+
+        $fragment.appendChild($newTr);
+        $carTable.get().appendChild($fragment);
+      },
+
+      //pega todos os valores digitados
+      getInputValues: function getInputValues() {
+        let $inputImagem = DOM("[data-js=imagem]").get().value;
+        let $inputModelo = DOM("[data-js=modelo]").get().value;
+        let $inputAno = DOM("[data-js=ano]").get().value;
+        let $inputPlaca = DOM("[data-js=placa]").get().value;
+        let $inputCor = DOM("[data-js=cor]").get().value;
+
+        return {
+          imagem: $inputImagem,
+          modelo: $inputModelo,
+          ano: $inputAno,
+          placa: $inputPlaca,
+          cor: $inputCor,
+        };
+      },
+    };
+  })();
 
   app.init();
 })(window.DOM, window, document);
